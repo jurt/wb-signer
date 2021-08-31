a190028a10ff31ba9f12bd345c75b96183a4003d
Alexey Samusenko <osam@travelnote.be>
2021-08-31 10:23:09 +0300

    initial implementation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,39 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'workbook-signer';
+    Array = Array;
+    window = window;
+
+    line1 = 'Зошит';
+    line2 = 'для робіт з %предмету%';
+    line3 = 'учня 4-В класу';
+    line4 = 'ЗОШ №18';
+    line5 = 'м. Біла Церква';
+    line6 = 'Іванова Вані';
+
+    addingDiscipline = false;
+
+
+    disciplines = [{
+        name: 'математики',
+        amount: 2
+    },{
+        name: 'української мови',
+        amount: 2
+    }];
+    newDisciplineName  = '';
+
+    add() {
+        this.disciplines.push({
+            name: this.newDisciplineName,
+            amount: 2
+        });
+        this.newDisciplineName = '';
+        this.addingDiscipline = false;
+    }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,37 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
 
-import { AppComponent } from './app.component';
+import {AppComponent} from './app.component';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatInputModule} from '@angular/material/input';
+import {FormsModule} from '@angular/forms';
+import {MatListModule} from '@angular/material/list';
+import {MatIconModule} from '@angular/material/icon';
+import {MatButtonModule} from '@angular/material/button';
+import {MatButtonToggleModule} from '@angular/material/button-toggle';
+import {FillPipe} from './fill.pipe';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+        FillPipe
+    ],
+    imports: [
+        BrowserModule,
+        BrowserAnimationsModule,
+        MatToolbarModule,
+        MatFormFieldModule,
+        MatInputModule,
+        FormsModule,
+        MatListModule,
+        MatIconModule,
+        MatButtonModule,
+        MatButtonToggleModule
+    ],
+    providers: [],
+    bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
